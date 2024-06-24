eccc6a1010888b32dce0bf2b79aae7bb919b935a
M-Usama1-Coder <uk7993572@gmail.com>
2024-06-24 23:07:15 +0500

    create-user-profile-update

--- src/routes/MyUserRoutes.ts
+++ src/routes/MyUserRoutes.ts
@@ -1,8 +1,11 @@
-import express from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import express from "express";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put("/", validateMyUserRequest ,jwtParse, jwtCheck, MyUserController.updateCurrentUser);
 
 export default router;
